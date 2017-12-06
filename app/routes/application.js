19a4b7b0c692034a5dc8ce2ba8ef9331a5587e80
Ларькин Олег Иванович <o.larkin@bssys.com>
2017-12-06 16:10:09 +0300

    #11 pretty notifications added

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,8 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  notify: service(),
+  notificationTypes: ['info', 'success', 'warning', 'danger'],
   session: service(),
   beforeModel(transition) {
     return this.get('session').fetch()
@@ -16,8 +18,14 @@ export default Route.extend({
     signOut: function() {
       this.get('session').close();
     },
-    notify(type, message) {
-      alert(message);
+    error(e) {
+      alert(e);
     },
+    notify(type, text) {
+      if (this.get('notificationTypes').indexOf(type) === -1) {
+        return this.send('error', text);
+      }
+      return this.get('notify')[type](text);
+    }
   }
 });
