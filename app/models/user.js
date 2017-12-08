f6c57702ca777806ced108eec3f19aa2fb216188
Ларькин Олег Иванович <o.larkin@bssys.com>
2017-12-08 19:08:42 +0300

    user exp points added

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,11 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
+
+const poinsMap = {
+  low: 10,
+  normal: 20,
+  high: 30,
+};
 
 export default DS.Model.extend({
   name: DS.attr('string'),
@@ -7,4 +14,12 @@ export default DS.Model.extend({
 
   cheevies: DS.hasMany('cheevie'),
   badges: DS.hasMany('badge'),
+
+  exp: computed('cheevies', 'badges', function() {
+    const exp = this.get('cheevies').reduce((sum, item) => {
+      return sum + poinsMap[item.get('power')];
+    }, 0);
+
+    return exp;
+  }),
 });
