c4f81b87c544e11c3834c9807ed3baa24cc125e7
Oleg Larkin <olegollar@gmail.com>
2018-01-13 03:07:31 +0300

    pushes seems to work

--- functions/index.js
+++ functions/index.js
@@ -9,17 +9,29 @@ admin.initializeApp(functions.config().firebase);
 //  response.send("Hello from Firebase!");
 // });
 
-exports.onAddCheevie = functions.database.ref('/users/{userId}/cheevies').onUpdate(event => {
-  const value = event.data.previous.val();
+exports.onAddCheevie = functions.database.ref('/users/{userId}/cheevies/{cheevieId}').onCreate(event => {
+  const userId = event.params.userId;
+  const cheevieId = event.params.cheevieId;
+
   const getDeviceTokensPromise = admin.database().ref(`/users/${userId}/fcmToken`).once('value');
+  const getCheeviePromise = admin.database().ref(`/cheevies/${cheevieId}`).once('value');
 
-  const payload = {
-    notification: {
-      title: 'You have a new cheevie!',
-      // body: `${follower.displayName} is now following you.`,
-      // icon: follower.photoURL
-    }
-  };
+  return Promise.all([getDeviceTokensPromise, getCheeviePromise]).then(results => {
+    let fcmToken = results[0].val();
+    let cheevie = results[1].val();
 
-  console.log(value);
+    if (!fcmToken) return;
+
+    const payload = {
+      notification: {
+        title: 'У тебя новая ачивка!',
+        body: `"${cheevie.name}" теперь твоя!`,
+        image: cheevie.imageUrl,
+        icon: '/manifest_icons/icon_192.png',
+        // badge: '/manifest_icons/icon_192.png',
+      }
+    };
+
+    return admin.messaging().sendToDevice(fcmToken, payload);
+  });
 });
