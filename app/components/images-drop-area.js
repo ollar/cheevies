b5a2d6012d08be684588f7f11fd6a7c83bcedcc4
Oleg Larkin <olegollar@gmail.com>
2018-01-15 23:34:38 +0300

    upload file process refactored

--- app/components/images-drop-area.js
+++ app/components/images-drop-area.js
@@ -33,8 +33,13 @@ export default Component.extend({
       e.preventDefault();
       this.set('dragover', false);
       const files = Array.prototype.slice.call(e.dataTransfer.files);
+      this.sendAction('uploadImage', files[0]);
+    },
 
-      this.sendAction('uploadImage', files[0])
+    inputChange(e) {
+      e.preventDefault();
+      const files = Array.prototype.slice.call(e.target.files);
+      this.sendAction('uploadImage', files[0]);
     },
 
     removeImage(imageModel) {
