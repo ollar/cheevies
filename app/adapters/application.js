dc2b536c3c942512fca16b50e866963ad53a2bd1
Oleg Larkin <oleg.larkin@nordigy.ru>
2018-07-06 18:03:32 +0300

    adapter refresh dev

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,8 @@
 import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 export default FirebaseAdapter.extend({
+  shouldBackgroundReloadAll: () => true,
+  shouldBackgroundReloadRecord: () => true,
+  shouldReloadAll: () => false,
+  shouldReloadRecord: () => false,
 });
