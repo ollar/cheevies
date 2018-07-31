b1767d745ce52c7e601c0f4ffc24fa3f5d484a6e
Oleg Larkin <olegollar@gmail.com>
2018-08-01 01:14:08 +0300

    index page dev 2

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,12 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   activePage: 'users',
+
+  users: computed('model.users.[]', function() {
+    return this.model.users.filter(user => user.id !== this.model.me.id);
+  }),
+
+  cheevies: computed.readOnly('model.cheevies'),
 });
