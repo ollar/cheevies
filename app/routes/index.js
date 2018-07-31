71c6a1913eea443a264ed8911716a9d091bfeaea
Oleg Larkin <olegollar@gmail.com>
2018-08-01 00:54:55 +0300

    index page dev

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,5 @@
 import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
-import { schedule } from '@ember/runloop';
-import Middleware from 'web-animation-middleware';
 import AuthenticatedRouteMixin from '../mixins/authenticated-route-mixin';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
@@ -26,28 +24,5 @@ export default Route.extend(AuthenticatedRouteMixin, {
           cheevies: group.get('firstObject.cheevies'),
         })
       );
-  },
-
-  activate() {
-    const am = new Middleware();
-    schedule('afterRender', () => {
-      const $iconImages = document.querySelectorAll('.icon-image');
-      am.prepare($iconImages, { transform: 'scale(0.5)', opacity: 0 });
-
-      am.chain(
-        $iconImages,
-        [
-          { transform: 'scale(0.5)', opacity: 0 },
-          { transform: 'scale(1)', opacity: 1 },
-        ],
-        {
-          duration: 24,
-          fill: 'forwards',
-        }
-      );
-      schedule('afterRender', () => {
-        // am.go(() => true);
-      });
-    });
   },
 });
