255f4fc714e78d0c0309a1bfaccbe4f3adeff3a3
Oleg Larkin <olegollar@gmail.com>
2018-08-02 01:53:47 +0300

    index page dev 10

---
+++ app/components/site-drawer-aside.js
@@ -0,0 +1,10 @@
+import Component from 'site-drawer-component/components/site-drawer-aside';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { hash } from 'rsvp';
+
+export default Component.extend({
+  me: service(),
+  avatar: computed.readOnly('me.model.image-set'),
+  cheevies: computed.readOnly('me.model.cheevies'),
+});
--- app/components/site-header.js
+++ app/components/site-header.js
@@ -1,26 +1,12 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: 'header',
   classNames: ['header'],
 
-  session: service(),
-  me: service(),
-  router: service(),
-
-  user: computed('me.model', function() {
-    if (this.get('me.model')) return this.get('me.model');
-    return {};
-  }),
-  avatar: computed.readOnly('user.image-set.64'),
-
   actions: {
-    signOut() {
-      this.get('session')
-        .invalidate()
-        .then(() => this.get('router').transitionTo('login'));
+    toggleDrawer() {
+      this._drawerData.toggleDrawer();
     },
   },
 });
