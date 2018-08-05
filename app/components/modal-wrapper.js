dd02dad8a455ebae15b482dbfb67414ffef3ec66
Oleg Larkin <olegollar@gmail.com>
2018-08-05 21:16:12 +0300

    modal dev

--- app/components/modal-wrapper.js
+++ app/components/modal-wrapper.js
@@ -1,17 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  classNames: ['modal'],
   didInsertElement() {
-    this.$('.modal-background')[0].animate([
-      { opacity: 0 },
-      { opacity: 1 },
-    ], 100);
-
-    this.$('.modal-content')[0].animate([
-      { transform: 'scale(0.8)' },
-      { transform: 'scale(1)' },
-    ], 50);
-
     this._super(...arguments);
+
+    this.element.classList.add('is-active');
   },
 });
