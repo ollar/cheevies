2c14dad90249aa2bde05ec596f355dd4a2f2c614
Oleg Larkin <olegollar@gmail.com>
2018-08-07 09:00:47 +0300

    user icons dev

--- app/components/icon-image.js
+++ app/components/icon-image.js
@@ -3,6 +3,15 @@ import Component from 'avatar-icon-component/components/avatar-icon';
 import { isEmpty } from '@ember/utils';
 
 export default Component.extend({
+  colours: computed(() => [
+    ['#FF0000', '#4838FF'],
+    ['#4838FF', '#1D2349'],
+    ['#CD04FF', '#BA0064'],
+    ['#0075FF', '#FF008A'],
+    ['#FF5C00', '#FF0000'],
+    ['#FFEC87', '#FF0000'],
+  ]),
+
   name: computed('data.name', function() {
     if (isEmpty(this.data)) return '';
     return this.getWithDefault('data.name', 'anonymous');
@@ -17,7 +26,10 @@ export default Component.extend({
 
   updateStyles() {
     this.$().css({
-      backgroundColor: this.get('backgroundColour'),
+      background: `linear-gradient(210deg,
+        ${this.get('backgroundColour')[0]},
+        ${this.get('backgroundColour')[1]}
+      )`,
       height: this._size,
       width: this._size,
       lineHeight: this._size + 'px',
