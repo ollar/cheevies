638dbb31f73bc01400a83718b1b218127321c0db
Oleg Larkin <olegollar@gmail.com>
2018-08-15 00:41:20 +0300

    sidebar cheevies list dev

--- app/components/site-drawer-aside.js
+++ app/components/site-drawer-aside.js
@@ -1,13 +1,27 @@
 import Component from 'site-drawer-component/components/site-drawer-aside';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
+import DS from 'ember-data';
 
 export default Component.extend({
   me: service(),
   session: service(),
+  myGroup: service(),
   router: service(),
   avatar: computed.readOnly('me.model.image-set'),
-  cheevies: computed.readOnly('me.model.cheevies'),
+
+  cheevies: computed('me.model.cheevies.[]', function() {
+    return DS.PromiseArray.create({
+      promise: this.get('myGroup')
+        .fetch()
+        .then(myGroup => myGroup.get('cheevies'))
+        .then(availableCheevies =>
+          this.me.model.cheevies.filter(
+            cheevie => availableCheevies.indexOf(cheevie) > -1
+          )
+        ),
+    });
+  }),
 
   actions: {
     createCheevie() {
