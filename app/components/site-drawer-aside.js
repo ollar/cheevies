cd54ae6ebdb915ef75c44b26ab8a9d40682739b3
Oleg Larkin <olegollar@gmail.com>
2018-10-01 23:23:30 +0300

    #49 sidebar cheevies load fix

--- app/components/site-drawer-aside.js
+++ app/components/site-drawer-aside.js
@@ -5,33 +5,33 @@ import DS from 'ember-data';
 import { resolve } from 'rsvp';
 
 export default Component.extend({
-  me: service(),
-  myGroup: service(),
-  router: service(),
-  avatar: computed.readOnly('me.model.image-set'),
+    me: service(),
+    myGroup: service(),
+    router: service(),
+    avatar: computed.readOnly('me.model.image-set'),
 
-  cheevies: computed('me.model.cheevies.[]', function() {
-    return DS.PromiseArray.create({
-      promise: this.get('myGroup')
-        .fetch()
-        .then(myGroup => {
-          if (!myGroup) return resolve([]);
-          return myGroup.get('cheevies');
-        })
-        .then(availableCheevies =>
-          this.getWithDefault('me.model.cheevies', []).filter(
-            cheevie => availableCheevies.indexOf(cheevie) > -1
-          )
-        ),
-    });
-  }),
+    cheevies: computed('me.model.cheevies.[]', 'myGroup.groupName', function() {
+        return DS.PromiseArray.create({
+            promise: this.get('myGroup')
+                .fetch()
+                .then(myGroup => {
+                    if (!myGroup) return resolve([]);
+                    return myGroup.get('cheevies');
+                })
+                .then(availableCheevies =>
+                    this.getWithDefault('me.model.cheevies', []).filter(
+                        cheevie => availableCheevies.indexOf(cheevie) > -1
+                    )
+                ),
+        });
+    }),
 
-  actions: {
-    createCheevie() {
-      this.get('router').transitionTo('index.create-cheevie');
+    actions: {
+        createCheevie() {
+            this.get('router').transitionTo('index.create-cheevie');
+        },
+        invalidate() {
+            return this.get('router').transitionTo('logout');
+        },
     },
-    invalidate() {
-      return this.get('router').transitionTo('logout');
-    },
-  },
 });
