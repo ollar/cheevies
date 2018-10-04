4ffce9ebeca8904072f0941e3b369acfa01fe0b6
Oleg Larkin <olegollar@gmail.com>
2018-10-05 01:15:56 +0300

    #50 done 2

--- app/components/props-container.js
+++ app/components/props-container.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import Middleware from 'web-animation-middleware';
 
 export default Component.extend({
     shuffleProps() {
@@ -6,10 +7,21 @@ export default Component.extend({
         const height = document.body.scrollHeight;
         const width = document.body.scrollWidth;
 
+        const am = new Middleware();
+
         $props.forEach($prop => {
-            $prop.style.transform = `translate(${Math.random() * width}px, -${Math.random() *
-                height}px)`;
+            am.prepare($prop, {
+                transform: `translate(${Math.random() * width}px, -${Math.random() * height}px)`,
+                opacity: 0,
+            });
         });
+
+        am.chain($props, [{ opacity: 0 }, { opacity: 1 }], {
+            duration: 300,
+            fill: 'forwards',
+        });
+
+        am.go(() => true);
     },
 
     didInsertElement() {
