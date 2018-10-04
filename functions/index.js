9de71a4737ed0a661fbbb46f035c628595f0c63c
Oleg Larkin <olegollar@gmail.com>
2018-10-04 23:51:45 +0300

    notification function dev 1

--- functions/index.js
+++ functions/index.js
@@ -11,119 +11,117 @@ const storageBucket = 'cheevies-jerk.appspot.com';
 // });
 
 exports.removeImageFileOnModelDestroy = functions.database
-  .ref('/images/{imageId}')
-  .onDelete(snapshot => {
-    const value = snapshot.val();
+    .ref('/images/{imageId}')
+    .onDelete(snapshot => {
+        const value = snapshot.val();
 
-    if (value) {
-      return admin
-        .storage()
-        .bucket(storageBucket)
-        .file(value.fullPath)
-        .delete();
-    }
-    return false;
-  });
+        if (value) {
+            return admin
+                .storage()
+                .bucket(storageBucket)
+                .file(value.fullPath)
+                .delete();
+        }
+        return false;
+    });
 
 exports.removeImageSetOnCheevieModelDestroy = functions.database
-  .ref('/cheevies/{id}')
-  .onDelete(snapshot => {
-    const cheevie = snapshot.val();
-    const imageSet = cheevie['image-set'];
+    .ref('/cheevies/{id}')
+    .onDelete(snapshot => {
+        const cheevie = snapshot.val();
+        const imageSet = cheevie['image-set'];
 
-    if (imageSet) {
-      return admin
-        .database()
-        .ref('/imageSets/' + imageSet)
-        .remove();
-    }
-    return false;
-  });
+        if (imageSet) {
+            return admin
+                .database()
+                .ref('/imageSets/' + imageSet)
+                .remove();
+        }
+        return false;
+    });
 
 exports.removeImageModelsOnImageSetModelDestroy = functions.database
-  .ref('/imageSets/{setId}')
-  .onDelete(snapshot => {
-    const imageSet = snapshot.val();
+    .ref('/imageSets/{setId}')
+    .onDelete(snapshot => {
+        const imageSet = snapshot.val();
 
-    if (imageSet) {
-      return Promise.all(
-        Object.keys(imageSet).map(setKey =>
-          admin
-            .database()
-            .ref('/images/' + imageSet[setKey])
-            .remove()
-        )
-      );
-    }
-    return false;
-  });
+        if (imageSet) {
+            return Promise.all(
+                Object.keys(imageSet).map(setKey =>
+                    admin
+                        .database()
+                        .ref('/images/' + imageSet[setKey])
+                        .remove()
+                )
+            );
+        }
+        return false;
+    });
 
 exports.createUserModelOnSignUp = functions.auth.user().onCreate(user => {
-  var { uid, email, displayName } = user;
-  return admin
-    .database()
-    .ref('/users/' + uid)
-    .set({
-      name: displayName,
-      email,
-      created: Date.now(),
-    });
+    var { uid, email, displayName } = user;
+    return admin
+        .database()
+        .ref('/users/' + uid)
+        .set({
+            name: displayName,
+            email,
+            created: Date.now(),
+        });
 });
 
 exports.removeUserModelOnDelete = functions.auth.user().onDelete(user => {
-  var { uid } = user;
-  return admin
-    .database()
-    .ref('/users/' + uid)
-    .remove();
+    var { uid } = user;
+    return admin
+        .database()
+        .ref('/users/' + uid)
+        .remove();
 });
 
 exports.onAddCheevie = functions.database
-  .ref('/users/{userId}/cheevies/{cheevieId}')
-  .onCreate(event => {
-    const userId = event.params.userId;
-    const cheevieId = event.params.cheevieId;
+    .ref('/users/{userId}/cheevies/{cheevieId}')
+    .onCreate((cheevie, event) => {
+        const userId = event.params.userId;
+        const cheevieId = event.params.cheevieId;
 
-    const getDeviceTokensPromise = admin
-      .database()
-      .ref(`/users/${userId}/fcmToken`)
-      .once('value');
-    const getCheeviePromise = admin
-      .database()
-      .ref(`/cheevies/${cheevieId}`)
-      .once('value');
+        const getDeviceTokensPromise = admin
+            .database()
+            .ref(`/users/${userId}/fcmToken`)
+            .once('value');
+        const getCheeviePromise = admin
+            .database()
+            .ref(`/cheevies/${cheevieId}`)
+            .once('value');
 
-    return Promise.all([getDeviceTokensPromise, getCheeviePromise]).then(
-      results => {
-        let fcmToken = results[0].val();
-        let cheevie = results[1].val();
+        return Promise.all([getDeviceTokensPromise, getCheeviePromise]).then(results => {
+            let fcmToken = results[0].val();
+            let cheevie = results[1].val();
 
-        if (!fcmToken) return;
+            if (!fcmToken) return;
 
-        const payload = {
-          notification: {
-            title: 'У тебя новая ачивка!',
-            body: `"${cheevie.name}" теперь твоя!`,
-            image: cheevie.imageUrl,
-            icon: cheevie.imageUrl,
-          },
-        };
+            const payload = {
+                notification: {
+                    title: "Hooray! You've got new cheevie",
+                    body: `"${cheevie.name}" is yours!`,
+                    image: cheevie.imageUrl,
+                    icon: cheevie.imageUrl,
+                },
+            };
 
-        return admin.messaging().sendToDevice(fcmToken, payload);
-      }
-    );
-  });
+            return admin.messaging().sendToDevice(fcmToken, payload);
+        });
+    });
 
 exports.addUnseenCheevieOnAddCheevie = functions.database
-  .ref('/users/{userId}/cheevies/{cheevieId}')
-  .onCreate((snapshot, event) => {
-    const userId = event.params.userId;
-    const cheevieId = event.params.cheevieId;
+    .ref('/users/{userId}/cheevies/{cheevieId}')
+    .onCreate((snapshot, event) => {
+        const userId = event.params.userId;
+        const cheevieId = event.params.cheevieId;
 
-    return admin
-      .database()
-      .ref('/users/' + userId + '/unseenCheevies')
-      .update({
-        [cheevieId]: true,
-      });
-  });
+        return admin
+            .database()
+            .ref('/users/' + userId + '/unseenCheevies')
+            .update({
+                [cheevieId]: true,
+            });
+    });
