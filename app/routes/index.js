494981b55f6592b5f18905a7ff4a884fa01e1728
Oleg Larkin <olegollar@gmail.com>
2018-10-10 22:05:16 +0300

    #46 fetch images models on index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,18 +4,25 @@ import AuthenticatedRouteMixin from '../mixins/authenticated-route-mixin';
 import { inject as service } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-  me: service(),
-  myGroup: service('my-group'),
+    me: service(),
+    myGroup: service('my-group'),
 
-  model() {
-    if (!this.get('myGroup.groupName')) return {};
+    model() {
+        if (!this.get('myGroup.groupName')) return {};
 
-    return this.myGroup.fetch().then(group =>
-      hash({
-        me: this.me.fetch(),
-        users: group.get('users'),
-        cheevies: group.get('cheevies'),
-      })
-    );
-  },
+        return this.myGroup.fetch().then(group =>
+            hash({
+                me: this.me.fetch(),
+                users: group.get('users'),
+                cheevies: group.get('cheevies'),
+            })
+        );
+    },
+
+    afterModel() {
+        const imageSets = this.store.peekAll('image-set');
+        imageSets.forEach(_is =>
+            _is.eachRelationship(key => requestIdleCallback(() => _is.get(key)))
+        );
+    },
 });
