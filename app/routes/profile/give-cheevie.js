e962dbf824fca1aa1e79bf8e7a4368e3f9e4bc2f
Oleg Larkin <olegollar@gmail.com>
2018-10-12 09:37:02 +0300

    #84 fix

--- app/routes/profile/give-cheevie.js
+++ app/routes/profile/give-cheevie.js
@@ -3,12 +3,18 @@ import { inject as service } from '@ember/service';
 import { hash } from 'rsvp';
 
 export default Route.extend({
-  myGroup: service(),
+    myGroup: service(),
 
-  model() {
-    return hash({
-      user: this.modelFor('profile'),
-      cheevies: this.myGroup.fetch().then(group => group.get('cheevies')),
-    });
-  },
+    model() {
+        return hash({
+            user: this.modelFor('profile'),
+            cheevies: this.myGroup.fetch().then(group => group.get('cheevies')),
+        }).then(({ user, cheevies }) => {
+            const userCheevies = user.get('cheevies');
+            return {
+                cheevies: cheevies.filter(cheevie => userCheevies.indexOf(cheevie) < 0),
+                user,
+            };
+        });
+    },
 });
