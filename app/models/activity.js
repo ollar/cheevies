b434c634d7551dd1b8ce4ef2d589a264e0a39ca4
Oleg Larkin <olegollar@gmail.com>
2018-10-17 22:48:23 +0300

    #88 activity model added

--- app/models/activity.js
+++ app/models/activity.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    group: DS.belongsTo('group'),
+    user: DS.belongsTo('user'),
+    cheevie: DS.belongsTo('cheevie'),
+    data: DS.attr(),
 });
