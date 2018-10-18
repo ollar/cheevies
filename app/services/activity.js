1cccaabaa7d615a02db85ab6561ff30e6ad21f09
Oleg Larkin <olegollar@gmail.com>
2018-10-18 22:07:28 +0300

    #80 mirage added

--- app/services/activity.js
+++ app/services/activity.js
@@ -1,4 +1,18 @@
-import Service from '@ember/service';
+import Service, { inject as service } from '@ember/service';
 
 export default Service.extend({
+    store: service(),
+    me: service(),
+    myGroup: service('my-group'),
+
+    pushActivity(data) {
+        const activity = this.store.createRecord('activity', {
+            group: this.myGroup.model,
+            user: this.me.model,
+            created: Date.now(),
+            data,
+        });
+
+        return activity.save();
+    },
 });
