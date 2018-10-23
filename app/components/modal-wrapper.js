5cebf9b1c5492530bea072a2a61d370212c481a8
Oleg Larkin <olegollar@gmail.com>
2018-10-24 00:22:00 +0300

    modal animations. give cheevie

--- app/components/modal-wrapper.js
+++ app/components/modal-wrapper.js
@@ -12,11 +12,19 @@ export default Component.extend(BusyMixin, {
     didInsertElement() {
         this._super(...arguments);
 
-        TweenLite.from('.modal-content', this.animationDuration, {
-            y: 20,
-            opacity: 0,
-            ease: Power4.easeOut,
-        });
+        if (this.giveCheevieModal) {
+            TweenLite.from('.modal-content', this.animationDuration * 2, {
+                y: '100%',
+                opacity: 0,
+                ease: Power4.easeOut,
+            });
+        } else {
+            TweenLite.from('.modal-content', this.animationDuration, {
+                y: 20,
+                opacity: 0,
+                ease: Power4.easeOut,
+            });
+        }
         TweenLite.from('.modal-background,.modal-close', this.animationDuration, {
             opacity: 0,
         });
@@ -24,14 +32,24 @@ export default Component.extend(BusyMixin, {
 
     actions: {
         goBack() {
-            TweenLite.to('.modal-content', this.animationDuration, {
-                y: 20,
-                opacity: 0,
-                onComplete: () => {
-                    if (this.goBack && this.goBack.call) this.goBack();
-                },
-                ease: Power4.easeOut,
-            });
+            const onComplete = () => {
+                if (this.goBack && this.goBack.call) this.goBack();
+            };
+            if (this.giveCheevieModal) {
+                TweenLite.to('.modal-content', this.animationDuration * 2, {
+                    y: '100%',
+                    opacity: 0,
+                    onComplete,
+                    ease: Power4.easeIn,
+                });
+            } else {
+                TweenLite.to('.modal-content', this.animationDuration, {
+                    y: 20,
+                    opacity: 0,
+                    onComplete,
+                    ease: Power4.easeOut,
+                });
+            }
             TweenLite.to('.modal-background,.modal-close', this.animationDuration, {
                 opacity: 0,
             });
