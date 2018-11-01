9244e75f0085b3afec50dc5cbb1b1aad8fa902d8
Oleg Larkin <oleg.larkin@nordigy.ru>
2018-11-01 15:30:40 +0300

    travis dev

--- patch_version.sh
+++ patch_version.sh
@@ -5,6 +5,6 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git checkout ${TRAVIS_BRANCH}
 
-npm version patch -m "Version up to %s"
+npm version patch -m "Version up to %s [skip travis]"
 
 git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}
