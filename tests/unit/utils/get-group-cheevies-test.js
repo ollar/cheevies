90272193b4598437b2759951108bf1d5ac75b7ce
Oleg Larkin <olegollar@gmail.com>
2018-11-11 14:52:48 +0300

    #114 group get cheevies util tests added

--- tests/unit/utils/get-group-cheevies-test.js
+++ tests/unit/utils/get-group-cheevies-test.js
@@ -1,11 +1,38 @@
 import getGroupCheevies from 'cheevies-jerk/utils/get-group-cheevies';
 import { module, test } from 'qunit';
+import { resolve } from 'rsvp';
+
+import EmberObject from '@ember/object';
+
+const Group = EmberObject.extend({
+    cheevies: resolve([]),
+});
 
 module('Unit | Utility | get-group-cheevies', function(hooks) {
+    // Replace this with your real tests.
+    test('it works', async function(assert) {
+        const group = Group.create();
 
-  // Replace this with your real tests.
-  test('it works', function(assert) {
-    let result = getGroupCheevies();
-    assert.ok(result);
-  });
+        let result = await getGroupCheevies(group);
+        assert.ok(result);
+    });
+
+    test('it gets cheevies', async function(assert) {
+        const group = Group.create();
+        group.set(
+            'cheevies',
+            resolve([{ deleted: false }, { deleted: false }, { deleted: false }])
+        );
+
+        let result = await getGroupCheevies(group);
+        assert.ok(result);
+    });
+
+    test('it filters cheevies', async function(assert) {
+        const group = Group.create();
+        group.set('cheevies', resolve([{ deleted: true }, { deleted: false }, { deleted: false }]));
+
+        let result = await getGroupCheevies(group);
+        assert.equal(result.length, 2);
+    });
 });
