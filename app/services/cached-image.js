d95f260ff2b410831c23cb2f3c58e2645171683c
Oleg Larkin <olegollar@gmail.com>
2018-11-12 09:52:23 +0300

    #107 fetch img service dev 1

---
+++ app/services/cached-image.js
@@ -0,0 +1,31 @@
+import Service from '@ember/service';
+import { getOwner } from '@ember/application';
+
+export default Service.extend({
+    appName() {
+        return getOwner(this).application.name;
+    },
+
+    getCachedSrc(_src) {
+        if (!_src) return;
+        const filePath = `${this.appName()}::${_src}`;
+
+        if (localStorage.getItem(filePath)) return localStorage.getItem(filePath);
+
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = function() {
+            var blob = xhr.response;
+            var reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onloadend = function() {
+                var base64data = reader.result;
+                localStorage.setItem(filePath, base64data);
+            };
+        };
+        xhr.open('GET', _src);
+        xhr.send();
+
+        return _src;
+    },
+});
