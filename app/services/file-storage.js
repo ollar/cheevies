03de11fe042df346a40e906ae1b71502eb177b68
Oleg Larkin <olegollar@gmail.com>
2018-11-14 23:41:59 +0300

    firebase cache control added

--- app/services/file-storage.js
+++ app/services/file-storage.js
@@ -3,23 +3,26 @@ import Service from '@ember/service';
 import firebase from 'firebase';
 
 export default Service.extend({
-  storageRef: computed(function() {
-    return firebase.storage();
-  }),
+    storageRef: computed(function() {
+        return firebase.storage();
+    }),
 
-  upload(path, file) {
-    return this.get('storageRef')
-      .ref(path)
-      .put(file)
-      .then(snapshot => snapshot.metadata);
-  },
+    upload(path, file) {
+        const metadata = {
+            cacheControl: 'public,max-age=31536000',
+        };
+        return this.get('storageRef')
+            .ref(path)
+            .put(file, metadata)
+            .then(snapshot => snapshot.metadata);
+    },
 
-  remove(imageModel, path) {
-    const _path = (imageModel && imageModel.get('fullPath')) || path;
+    remove(imageModel, path) {
+        const _path = (imageModel && imageModel.get('fullPath')) || path;
 
-    if (!_path) throw new Error('no file path specified');
-    return this.get('storageRef')
-      .ref(_path)
-      .delete();
-  },
+        if (!_path) throw new Error('no file path specified');
+        return this.get('storageRef')
+            .ref(_path)
+            .delete();
+    },
 });
