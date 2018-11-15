a70802bb037c0a86b85fec5ddb8d6951a71162a4
Oleg Larkin <olegollar@gmail.com>
2018-11-16 00:46:02 +0300

    #119 cheevie details drag disabled on edit

--- app/components/modal-content.js
+++ app/components/modal-content.js
@@ -9,11 +9,13 @@ export default Component.extend(DraggableMixin, {
     },
 
     handlePanMove(ev) {
+        if (this.disableDrag) return;
         if (this.giveCheevieModal && ev.deltaY < 0) return;
         this._super(ev);
     },
 
     onPanEnvComplete() {
+        if (this.disableDrag) return;
         const moveY = this.initialTransform[1] - this.previousMoveY;
         if (Math.abs(moveY) > 50) {
             this.goBack();
