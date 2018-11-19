db5b5504fc9d50174d900ea4becc7f7902133289
Oleg Larkin <olegollar@gmail.com>
2018-11-19 22:47:41 +0300

    #122 give cheevie scroll bug

--- app/components/modal-content.js
+++ app/components/modal-content.js
@@ -3,21 +3,32 @@ import DraggableMixin from 'draggable-mixin/mixins/draggable';
 
 export default Component.extend(DraggableMixin, {
     classNames: ['modal-content'],
+    classNameBindings: ['panX:pan-x'],
 
     panDirection() {
         return this.DIRECTION_VERTICAL;
     },
 
+    handlePanStart(ev) {
+        const elWrapper = this.element.parentElement;
+        if (elWrapper.scrollTop === 0 && ev.direction === this.DIRECTION_DOWN) {
+            this.set('panX', true);
+        } else {
+            this.set('panX', false);
+        }
+        this._super(...arguments);
+    },
+
     handlePanMove(ev) {
-        if (this.disableDrag) return;
-        if (this.giveCheevieModal && ev.deltaY < 0) return;
+        if (this.disableDrag) return ev;
+        if (this.giveCheevieModal && ev.deltaY < 0) return ev;
         this._super(ev);
     },
 
     onPanEnvComplete() {
         if (this.disableDrag) return;
         const moveY = this.initialTransform[1] - this.previousMoveY;
-        if (Math.abs(moveY) > 50) {
+        if (Math.abs(moveY) > 100) {
             this.goBack();
             return;
         }
