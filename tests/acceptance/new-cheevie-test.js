9e6528b793c4fb126beffb2b368f464e6214d5c8
Oleg Larkin <olegollar@gmail.com>
2018-11-27 22:40:22 +0300

    new-cheevie flow tests added

--- tests/acceptance/new-cheevie-test.js
+++ tests/acceptance/new-cheevie-test.js
@@ -1,13 +1,32 @@
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
+import { testgroup, uid, testcheevie } from '../consts';
+
 module('Acceptance | new cheevie', function(hooks) {
-  setupApplicationTest(hooks);
+    setupApplicationTest(hooks);
 
-  skip('visiting /new-cheevie', async function(assert) {
-    await visit('/new-cheevie');
+    test('visiting / should redirect to /new-cheevie', async function(assert) {
+        const session = this.owner.lookup('service:session');
+        const me = this.owner.lookup('service:me');
+        const myGroup = this.owner.lookup('service:my-group');
 
-    assert.equal(currentURL(), '/new-cheevie');
-  });
+        await session.authenticate('authenticator:test', { uid });
+        session.set('data.group', testgroup);
+
+        await me.fetch();
+        await myGroup.fetch();
+
+        const unseenCheevie = this.owner
+            .lookup('service:store')
+            .createRecord('cheevie', { name: testcheevie });
+
+        me.model.get('unseenCheevies').addObject(unseenCheevie);
+        myGroup.model.get('cheevies').addObject(unseenCheevie);
+
+        await visit('/');
+
+        assert.equal(currentURL(), '/new-cheevies');
+    });
 });
--- tests/consts.js
+++ tests/consts.js
@@ -2,3 +2,4 @@ export const uid = 'aOku4UacsDeWnb5qezWOuw4EKvl2';
 export const testgroup = 'testgroup';
 export const email = 'tester@test.com';
 export const password = '123123123';
+export const testcheevie = 'testcheevie';
