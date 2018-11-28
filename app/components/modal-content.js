2cd038f62839b4099accca3ff279671b00c92e67
Oleg Larkin <olegollar@gmail.com>
2018-11-29 01:21:29 +0300

    #126 modal swipe bug fixed 1

--- app/components/modal-content.js
+++ app/components/modal-content.js
@@ -1,38 +1,71 @@
 import Component from '@ember/component';
 import DraggableMixin from 'draggable-mixin/mixins/draggable';
-
 export default Component.extend(DraggableMixin, {
     classNames: ['modal-content'],
-    classNameBindings: ['panAuto:pan-auto', 'disableDrag:pan-auto'],
+    // classNameBindings: ['panAuto:pan-auto', 'disableDrag:pan-auto'],
+
+    didInsertElement() {
+        this._super(...arguments);
+        this.elWrapper = this.element.parentElement;
+        this.elWrapperOffsetHeight = this.elWrapper.offsetHeight;
+        this.elementOffsetHeight = this.element.offsetHeight;
+    },
 
     panDirection() {
         return this.DIRECTION_VERTICAL;
     },
 
-    handlePanStart(ev) {
-        const elWrapper = this.element.parentElement;
-        if (elWrapper.scrollTop === 0 && ev.direction === this.DIRECTION_UP) {
-            this.set('panAuto', true);
-        } else {
-            this.set('panAuto', false);
-        }
-        this._super(...arguments);
-    },
-
     handlePanMove(ev) {
-        if (this.disableDrag) return ev;
-        if (this.giveCheevieModal && ev.deltaY < 0) return ev;
+        const transformY = this.initialTransform[1];
+        const moveY = transformY + ev.deltaY;
+
+        // if modal content is bigger than wrapper
+        if (this.elementOffsetHeight + 55 > this.elWrapperOffsetHeight) {
+            if (
+                this.giveCheevieModal &&
+                this.elementOffsetHeight + moveY + 55 < this.elWrapperOffsetHeight
+            )
+                return ev;
+        } else {
+            if (this.giveCheevieModal && moveY < 0) return ev;
+        }
+
         this._super(ev);
     },
 
     onPanEnvComplete() {
+        this.elWrapper = this.element.parentElement;
+
+        //remove it maybe
         if (this.disableDrag) return;
-        const moveY = this.initialTransform[1] - this.previousMoveY;
-        if (Math.abs(moveY) > 100) {
-            this.goBack();
-            return;
+
+        const transformY = this.initialTransform[1];
+        const moveY = transformY - this.previousMoveY;
+
+        // pan down
+        if (-transformY + moveY < 0) {
+            if (Math.abs(-transformY + moveY) > 100) {
+                this.goBack();
+                return;
+            }
         }
 
-        this._super(...arguments);
+        if (this.giveCheevieModal) return;
+
+        // pan up
+        // if modal content is bigger than wrapper
+        if (this.elementOffsetHeight + 55 > this.elWrapperOffsetHeight) {
+            if (this.elementOffsetHeight - this.previousMoveY - this.elWrapperOffsetHeight > 200) {
+                this.goBack();
+                return;
+            }
+        } else {
+            if (-transformY + moveY > 0) {
+                if (Math.abs(-transformY + moveY) > 100) {
+                    this.goBack();
+                    return;
+                }
+            }
+        }
     },
 });
