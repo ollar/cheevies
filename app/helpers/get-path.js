53e7527a3847a83f193a08e28f908475f668ecc1
Oleg Larkin <olegollar@gmail.com>
2018-12-06 23:03:29 +0300

    get-path helper tests fixed

--- app/helpers/get-path.js
+++ app/helpers/get-path.js
@@ -4,7 +4,7 @@ import ENV from 'cheevies-jerk/config/environment';
 const { rootURL } = ENV;
 
 export function getPath(params /*, hash*/) {
-    return rootURL + params.join('');
+    return rootURL + params.join('/');
 }
 
 export default helper(getPath);
--- tests/integration/helpers/get-path-test.js
+++ tests/integration/helpers/get-path-test.js
@@ -4,14 +4,20 @@ import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Helper | get-path', function(hooks) {
-  setupRenderingTest(hooks);
+    setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+    // Replace this with your real tests.
+    test('it renders', async function(assert) {
+        this.set('inputValue', '1234');
 
-    await render(hbs`{{get-path inputValue}}`);
+        await render(hbs`{{get-path inputValue}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
-  });
+        assert.equal(this.element.textContent.trim(), '/1234');
+    });
+
+    test('makes path', async function(assert) {
+        await render(hbs`{{get-path 'some-path' 'inner-dir' 'file-name.png'}}`);
+
+        assert.equal(this.element.textContent.trim(), '/some-path/inner-dir/file-name.png');
+    });
 });
