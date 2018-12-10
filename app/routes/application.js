7dbf38265ef9325972b3060838599ff9e92f6849
Oleg Larkin <olegollar@gmail.com>
2018-12-11 00:58:52 +0300

    #130 screen width notice added

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,14 +1,26 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { schedule } from '@ember/runloop';
 
 export default Route.extend({
     notify: service(),
+    i18n: service(),
     installStandalone: service('install-standalone'),
 
     init() {
         this._super(...arguments);
         this.notificationTypes = ['info', 'success', 'warning', 'error'];
         this.installStandalone.addListeners();
+    },
+    activate() {
+        if (!window.cordova && window.innerWidth > 1000) {
+            schedule('afterRender', () =>
+                this.send('notify', {
+                    type: 'warning',
+                    text: this.i18n.t('messages.screen_width_warning'),
+                })
+            );
+        }
     },
     actions: {
         notify({ type, text }) {
