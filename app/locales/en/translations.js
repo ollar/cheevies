d16e5f3477d86e9ea29b218f4f3b5a86d8ea7004
Oleg Larkin <olegollar@gmail.com>
2018-12-18 01:31:12 +0300

    #139 icons description 2

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -113,6 +113,9 @@ export default {
             animations: {
                 label: 'Animations',
             },
+            iconsDescription: {
+                label: 'Icons Descriptions',
+            },
             standaloneApp: {
                 label: 'Install App on Homescreen',
             },
--- app/routes/profile.js
+++ app/routes/profile.js
@@ -2,14 +2,14 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
-    me: service(),
+    settings: service(),
 
     userHasEnteredData() {
         return Object.keys(this.get('controller.model').changedAttributes()).length > 0;
     },
 
     activate() {
-        this.me.fetch();
+        this.settings.fetch();
     },
 
     actions: {
