bbbbaf40a9c61a27e05c29b46f1926a0f4b02cb4
Oleg Larkin <olegollar@gmail.com>
2018-12-19 01:26:06 +0300

    #143 group policy dev 1

--- app/models/group.js
+++ app/models/group.js
@@ -3,6 +3,8 @@ import Validator from '../mixins/model-validator';
 import { computed } from '@ember/object';
 
 export default DS.Model.extend(Validator, {
+    policies: computed(() => ['anarchy', 'democracy', 'totalitarianism']),
+
     name: DS.attr('string'),
     cheevies: DS.hasMany('cheevie'),
     users: DS.hasMany('user'),
@@ -10,6 +12,7 @@ export default DS.Model.extend(Validator, {
     code: DS.attr('string', { defaultValue: '0000' }),
     author: DS.belongsTo('user', { inverse: null }),
     moderators: DS.hasMany('user', { inverse: null }),
+    policy: DS.attr('string', { defaultValue: 'anarchy' }),
 
     validations: computed(() => ({
         name: {
