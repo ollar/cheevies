dfe31a12e9ec31a9e22bca10c7858f16497ffffe
Oleg Larkin <olegollar@gmail.com>
2018-12-25 00:43:56 +0300

    #143 group policy dev 3

---
+++ tests/unit/utils/user-role-test.js
@@ -0,0 +1,11 @@
+import userRole from 'cheevies-jerk/utils/user-role';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | user-role', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = userRole();
+    assert.ok(result);
+  });
+});
