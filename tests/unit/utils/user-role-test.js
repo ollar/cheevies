1924c28170886f17a84e353deb191332417b66b0
Oleg Larkin <olegollar@gmail.com>
2018-12-25 01:55:35 +0300

    #143 group policy tests fix

--- tests/unit/utils/user-role-test.js
+++ tests/unit/utils/user-role-test.js
@@ -1,11 +1,10 @@
 import userRole from 'cheevies-jerk/utils/user-role';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | user-role', function(hooks) {
-
-  // Replace this with your real tests.
-  test('it works', function(assert) {
-    let result = userRole();
-    assert.ok(result);
-  });
+module('Unit | Utility | user-role', function() {
+    // Replace this with your real tests.
+    test('it works', function(assert) {
+        let result = userRole();
+        assert.ok(result);
+    });
 });
