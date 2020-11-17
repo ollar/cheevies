73450338a961bd3f555ebb33d435a0478ef5638f
Oleg Larkin <olegollar@gmail.com>
2020-11-17 12:05:00 +0300

    dev

--- app/routes/index/activity.js
+++ app/routes/index/activity.js
@@ -1,8 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { schedule } from '@ember/runloop';
 
-// import { TimelineLite } from 'gsap';
 
 export default Route.extend({
     myGroup: service('my-group'),
@@ -10,21 +8,5 @@ export default Route.extend({
 
     model() {
         return this.activity.fetch();
-    },
-
-    activate() {
-        schedule('afterRender', () => {
-            // const tline = new TimelineLite({ delay: 0.2 });
-
-            // tline.staggerFrom(
-            //     '.activity-list-item',
-            //     0.3,
-            //     {
-            //         opacity: 0,
-            //         y: 10,
-            //     },
-            //     0.03
-            // );
-        });
     },
 });
